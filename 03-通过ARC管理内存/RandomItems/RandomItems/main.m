67eccdb181dc93c5a1de1ff8fe672ef82637c43a
joshua <king20130420@gmail.com>
2017-03-07 17:09:31 +0800

    通过ARC管理内存

---
+++ 03-通过ARC管理内存/RandomItems/RandomItems/main.m
@@ -0,0 +1,82 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Zeon Waa on 3/7/17.
+//  Copyright © 2017 Zeon Waa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ZWAItem.h"
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        //p36
+    
+        NSMutableArray *items = [[NSMutableArray alloc]init];
+//        [items addObject:@"One"];
+//        [items addObject:@"Two"];
+//        [items addObject:@"Three"];
+//        
+//        [items insertObject:@"Zero" atIndex:0];
+        
+        //遍历数组
+        //如果需要在循环中添加或删除对象，就不能使用枚举，否则程序会抛出异常。这时只能设置计数器并使用普通的for循环。
+//        for (NSString *item in items) {
+//            NSLog(@"%@",item);
+//        }
+        
+        // p45
+      //  ZWAItem *item = [[ZWAItem alloc]init];
+        
+//        [item setItemName:@"Red Sofa"];
+//        [item setSerialNumber:@"A1B2C"];
+//        [item setValueInDollars:100];
+        
+        //使用点语法存取实例变量
+//        item.itemName = @"Red Sofa";
+//        item.serialNumber = @"A1B2C";
+//        item.valueInDollars = 100;
+        
+       // NSLog(@"%@  %@  %@  %d",[item itemName],[item dateCreated],[item serialNumber],[item valueInDollars]);
+       // NSLog(@"%@  %@  %@  %d",item.itemName,item.dateCreated,item.serialNumber,item.valueInDollars);
+        
+        // p54
+        
+//        ZWAItem *item = [[ZWAItem alloc]initWithItemName:@"Red Sofa" valueInDollars:100 serialNumber:@"A1B2C"];
+//        NSLog(@"%@", item);
+//        
+//        ZWAItem *itemWithName = [[ZWAItem alloc]initWithItemName:@"Blue Sofa"];
+//        NSLog(@"%@", itemWithName);
+//        
+//        ZWAItem *itemWithNoName = [[ZWAItem alloc]init];
+//        NSLog(@"%@", itemWithNoName);
+
+//        for (int i = 0; i < 10; i++) {
+//            ZWAItem *item = [ZWAItem randomItem];
+//            [items addObject:item];
+//        }
+//        
+//        for (ZWAItem *item in items) {
+//            NSLog(@"%@", item);
+//        }
+        
+        // p71
+
+        ZWAItem *backpack = [[ZWAItem alloc]initWithItemName:@"Backpack"];
+        [items addObject:backpack];
+        
+        ZWAItem *caculator = [[ZWAItem alloc]initWithItemName:@"Calculator"];
+        [items addObject:caculator];
+        backpack.containedItem = caculator;
+        
+        backpack = nil;
+        caculator = nil;
+        
+        NSLog(@"Setting items to nil...");
+        items = nil;
+    }
+    return 0;
+}
