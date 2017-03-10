e9947bc101c7f8ded869fbcca6a914e3c2ee911c
joshua <king20130420@gmail.com>
2017-03-10 19:02:40 +0800

    UISplitViewController

---
+++ 22-UISplitViewController/Nerdfeed/Nerdfeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nerdfeed
+//
+//  Created by Zeon Waa on 3/10/17.
+//  Copyright © 2017 Zeon Waa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
